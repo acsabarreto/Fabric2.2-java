35218ed6611adb981b75c7dab5baafa3a00f8b6b
Antônio Sá Barreto <acsabarreto@gmail.com>
2025-07-16 18:12:47 -0300

    Chaincode App Update

---
+++ fabric-samples/chaincode/hometransfer/src/main/java/hometransfer/HomeTransferContractApp.java
@@ -0,0 +1,9 @@
+package hometransfer;
+
+import org.hyperledger.fabric.contract.ContractRouter;
+
+public class HomeTransferContractApp {
+    public static void main(String[] args) {
+        ContractRouter.main(args);
+    }
+}
